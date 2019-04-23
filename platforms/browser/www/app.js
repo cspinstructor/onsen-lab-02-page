ab7655976a877d947a407023746625ff23261fd6
unknown <student2@CL1-04.hju>
2019-04-23 17:11:28 +0800

    'update'

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -1,3 +1,20 @@
-function sayHello() {
-  ons.notification.alert("Hello world");
+function showAlert() {
+  ons.notification.alert('This is an alert');
+}
+
+var showConfirm = function() {
+  ons.notification.confirm('Please confirm:').then(function(input) {
+    ons.notification.alert('You clicked: ' + input);
+  });
+};
+
+var showPrompt = function() {
+  ons.notification.prompt('Prompt!').then(function(input) {
+    var message = input ? 'Entered: ' + input : 'Entered nothing!';
+    ons.notification.alert(message);
+  });
+};
+
+function showToast() {
+  ons.notification.toast('Toast!', { timeout: 2000 });
 }
